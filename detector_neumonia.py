f1bc1c08c4fc91b2abd8511923e2cf1b684a5e27
Laura Cardenas <79459341+carfolacam98@users.noreply.github.com>
2024-07-21 11:35:11 -0500

    Feature creating gradcam class

--- detector_neumonia.py
+++ detector_neumonia.py
@@ -87,9 +87,41 @@ class BacteriaPredictionLabels:
             return BacteriaLabels.bacteria_label_mapping[self.entry]
 
 
-def read_model():
-    model = tf.keras.models.load_model('conv_MLP_84.h5')
-    return model
+class Gradcam:
+    def read_model(self):
+        model = tf.keras.models.load_model('conv_MLP_84.h5')
+        return model
+
+    def __init__(self, array, preprocess_strategy: PreprocessStrategy):
+        self.img = preprocess_strategy.preprocess(array)
+        self.model = self.read_model()
+        self.array = array
+
+    def predict(self, img):
+        preds = self.model.predict(img)
+        argmax = np.argmax(preds[0])
+        output = self.model.output[:, argmax]
+        last_conv_layer = self.model.get_layer("conv10_thisone")
+        grads = K.gradients(output, last_conv_layer.output)[0]
+        pooled_grads = K.mean(grads, axis=(0, 1, 2))
+        iterate = K.function([self.model.input], [pooled_grads, last_conv_layer.output[0]])
+        pooled_grads_value, conv_layer_output_value = iterate(img)
+        for filters in range(64):
+            conv_layer_output_value[:, :, filters] *= pooled_grads_value[filters]
+        # creating the heatmap
+        heatmap = np.mean(conv_layer_output_value, axis=-1)
+        heatmap = np.maximum(heatmap, 0)  # ReLU
+        heatmap /= np.max(heatmap)  # normalize
+        heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[2]))
+        heatmap = np.uint8(255 * heatmap)
+        heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+        img2 = cv2.resize(self.array, (512, 512))
+        hif = 0.8
+        transparency = heatmap * hif
+        transparency = transparency.astype(np.uint8)
+        superimposed_img = cv2.add(transparency, img2)
+        superimposed_img = superimposed_img.astype(np.uint8)
+        return superimposed_img[:, :, ::-1]
 
 
 def grad_cam(array, preprocess_strategy: PreprocessStrategy):
